10f76bd4e68a0de3ba97f857c7c5b06e9d36db74
Pranay Kamble <iampranaykamble1@gmail.com>
2026-02-19 12:30:22 +0530

    Completed Lab-5/Exercise-1

---
+++ Lab-5/Exercise-1/script.js
@@ -0,0 +1,155 @@
+// Exercise 1: Employee Management System using Local XML
+// Scenario:
+// Create an Employee Management System that performs CRUD operations on employee data stored in a local XML file.
+// Requirements:
+// •	Create a local XML file employees.xml with fields:
+// o	Employee ID
+// o	Name
+// o	Department
+// o	Salary
+// •	Use AJAX to:
+// o	Fetch and display employee records in a table.
+// •	Parse XML response using:
+// o	responseXML
+// o	getElementsByTagName()
+// •	Implement CRUD operations:
+// o	Create → Add new employee node dynamically.
+// o	Read → Display all employees.
+// o	Update → Modify salary or department.
+// o	Delete → Remove employee node.
+// •	Use DOM manipulation to update UI dynamically.
+// •	Show proper success and error messages.
+// •	Handle empty XML or malformed XML errors.
+
+let timeOutId = 0;
+
+showStatus = (msg, status) => {
+    const element = document.querySelector("#status-message")
+    element.classList.remove('display-none')
+    element.textContent = `${status}: ${msg}`
+    element.style.color = (status === 200) ? "green" : "red"
+
+    if (timeOutId) clearTimeout(timeOutId)
+    timeOutId = setTimeout(() => {
+        element.classList.add('display-none')
+    }, 2000)
+}
+
+
+updateNode = (id) => {
+    const tr = document.querySelector(`#${id}`)
+    const td = tr.getElementsByTagName('td')
+
+    const salary = prompt('Enter new salary: '),
+          dept = prompt('Enter new department: ')
+
+    if (isNaN(Number(salary))) {
+        showStatus('Please enter numeric values for salary', 400);
+        return
+    }
+
+    if (Number(salary) <= 0) {
+        showStatus('Please enter positive value for salary', 400);
+        return
+    }
+
+
+
+    td[2].textContent = dept
+    td[3].textContent = salary
+}
+
+deleteNode = (id) => {
+    const tr = document.querySelector(`#${id}`)
+    tr.remove()
+
+    showStatus('Delete Successful', 200)
+}
+
+
+updateUI = (data) => {
+    const display = document.querySelector('#display')
+    display.classList.remove('display-none')
+    const employees = data.getElementsByTagName('employee')
+    const tbody = document.querySelector('tbody')
+    tbody.innerHTML = ""
+
+    for (let i=0; i<employees.length; ++i) {
+        const id = employees[i].getElementsByTagName('id')[0].textContent,
+              name = employees[i].getElementsByTagName('name')[0].textContent,
+              dept = employees[i].getElementsByTagName('department')[0].textContent,
+              salary = employees[i].getElementsByTagName('salary')[0].textContent
+
+        const tr = document.createElement('tr')
+        tr.id = `_${id}`
+        tr.innerHTML = `
+            <td>${id}</td>
+            <td>${name}</td>
+            <td>${dept}</td>
+            <td>${salary}</td>
+            <td> <button onclick="updateNode('_${id}')">Update</button>
+            <button onclick="deleteNode('_${id}')">Delete</button></td>
+        `
+
+        tbody.appendChild(tr)
+    }
+}
+
+
+load = () => {
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('GET','data.xml', true)
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = xhr.responseXML
+            updateUI(data)
+            showStatus("Fetch Successful",200);
+        }else if (xhr.readyState === 4){
+            showStatus(xhr.statusText, xhr.status)
+        }
+    }
+
+    xhr.send()
+}
+
+document.querySelector('#submit').addEventListener('click', (evt) => {
+    evt.preventDefault()
+    document.querySelector('#display').classList.remove('display-none')
+    const id = document.querySelector('#id').value,
+          name = document.querySelector('#name').value,
+          dept = document.querySelector('#dept').value,
+          salary = document.querySelector('#salary').value
+
+    if (id.trim() === '' || name.trim() === '' || dept.trim() === '') {
+        showStatus("All fields are required", 400)
+        return
+    }
+
+    if (salary <= 0) {
+        showStatus("Please enter positive value for salary", 400)
+        return
+    }
+
+    const tbody = document.querySelector('tbody'),
+          tr = document.createElement('tr')
+    
+    tr.id = `_${id}`
+    tr.innerHTML = `
+        <td>${id}</td>
+        <td>${name}</td>
+        <td>${dept}</td>
+        <td>${salary}</td>
+        <td> 
+            <button onclick="updateNode('_${id}')">Update</button>
+            <button onclick="deleteNode('_${id}')">Delete</button>
+        </td>
+    `
+    tbody.appendChild(tr)
+    showStatus('Create Successfull', 200);
+})
+
+
+document.querySelector('#view').addEventListener('click', (evt) => {
+    load()
+})
